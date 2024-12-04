caf291b7fe633f21ed037c3f406376b375852acf
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2024-12-04 20:19:42 +0530

    adding shabang in script

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,10 @@
 ####Sha-bang:- It is used to specify which shell is used to process our scripts.
-
+#!/bin/bash
 echo "helloworld"
 Person1=Ramesh
 Person2=Suresh
 
 echo "Ramesh: Hi Suresh,Good Morning"
 echo "Suresh:  Good Morning,Ramesh"
+
+
