bde1ff58bff8a7660230f6a97b0adc70b7c198f3
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2024-12-04 20:28:39 +0530

    how to add variable in script

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,13 +1,13 @@
 ####Sha-bang:- It is used to specify which shell is used to process our scripts.
 #!/bin/bash
 echo "helloworld"
-Person1=Ramesh
-Person2=Suresh
+PERSON1=Ramesh
+PERSON2=Suresh
 
 # echo "Ramesh: Hi Suresh,Good Morning"
 # echo "Suresh:  Good Morning,Ramesh"
 
 
 
- echo "$PERSON1: Hi $PERSON2,Good Morning"
-echo "$PERSON2:  Good Morning,$PERSON1"
+ echo "$PERSON1:  Hi $PERSON2,Good Morning"
+ echo "$PERSON2:  Good Morning,$PERSON1"
