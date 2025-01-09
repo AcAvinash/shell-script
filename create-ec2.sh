0da62d020c2b4a6f082dd1d27e5cb399289246d4
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2025-01-09 21:03:27 +0530

    adding script

--- create-ec2.sh
+++ create-ec2.sh
@@ -15,6 +15,23 @@ for i in "${NAMES[@]}"; do
     INSTANCE_TYPE="t2.micro"
   fi
   echo "creating $i instance"
-IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID  --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"  | jq -r '.Instances[0].PrivateIpAddress')
- echo " created $i instances: $IP_ADDRESS"
+
+# Store the output of the 'aws ec2 run-instances' command
+instance_info=$(aws ec2 run-instances --image-id "$IMAGE_ID" --instance-type "$INSTANCE_TYPE" --security-group-ids "$SECURITY_GROUP_ID" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]")
+
+# Check if the EC2 instance creation was successful
+if [ $? -eq 0 ]; then
+  # Extract the private IP address from the instance creation output
+  IP_ADDRESS=$(echo "$instance_info" | jq -r '.Instances[0].PrivateIpAddress')
+
+  # Check if the IP address was successfully extracted
+  if [ -z "$IP_ADDRESS" ]; then
+    echo "Failed to retrieve private IP address for $i instance."
+  else
+    echo "Created $i instance: $IP_ADDRESS"
+  fi
+else
+  echo "Failed to create $i instance."
+fi
+
 done
