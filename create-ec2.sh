67c99b73509076b7c493697de88e7a6bcd068019
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2025-01-09 20:47:00 +0530

    adding script

---
+++ create-ec2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#"rabbitmq" "catalogue" "user"  "shipping" "payment" "dispatch" "web" "mysql"
+
+NAMES=("mongodb" "cart")
+DOMAIN_NAME=joindevops.fun
+INSTANCE_TYPE=""
+IMAGE_ID=ami-09c813fb71547fc4f
+SECURITY_GROUP_ID=sg-0c82fa48dbc70749d 
+
+for i in "${NAMES[@]}"; do
+  if [[ $i == "mongodb" || $i == "mysql" ]]; then
+    INSTANCE_TYPE=t3.micro
+  else
+    INSTANCE_TYPE=t2.micro
+  fi
+  echo "creating $i instance"
+ IP_ADDRESS=(aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE --tag-specification "ResourceType=instance,Tags=[{key=Name,Value=$i}]" --security-group-ids $SECURITY_GROUP_ID | jq -r '.Instances[0].PrivateIpAddress')
+ echo " created $i instances: $IP_ADDRESS"
+done
