43a65c3c3524bf825bf32fd561215a6f3d87a752
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2025-01-16 20:47:31 +0530

    create-ec2.sh

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,5 +1,3 @@
-
-
 #!/bin/bash
 
 # Disable pager for AWS CLI to avoid interactive prompts
@@ -7,22 +5,16 @@ export AWS_PAGER=""
 
 # Define instance names and parameters
 NAMES=$@
-DOMAIN_NAME="joindevops.fun"
+DOMAIN_NAME=joindevops.fun
+INSTANCE_TYPE=""
 IMAGE_ID="ami-09c813fb71547fc4f"
-SECURITY_GROUP_ID="sg-0c82fa48dbc70749d"
-HOSTED_ZONE_ID="Z0798189H8VMAOYWAMIV"
+SECURITY_GROUP_ID=sg-0c82fa48dbc70749d
+HOSTED_ZONE_ID=Z0798189H8VMAOYWAMIV
 
 # Ensure AWS CLI is available by checking the PATH
 export PATH=$PATH:/usr/local/bin  # Adjust if necessary
 
-# Check if at least one name is provided
-if [ -z "$NAMES" ]; then
-  echo "No instance names provided. Usage: $0 instance_name1 instance_name2 ..."
-  exit 1
-fi
-
-# Iterate over provided instance names
-for i in $NAMES; do
+for i in $@; do
   # Set instance type based on name
   if [[ $i == "mongodb" || $i == "mysql" ]]; then
     INSTANCE_TYPE="t3.micro"
@@ -31,48 +23,15 @@ for i in $NAMES; do
   fi
 
   # Display instance creation details
-  echo "Creating $i instance type: $INSTANCE_TYPE"
+  echo "creating $i instance type: $INSTANCE_TYPE"
 
   # Run AWS EC2 instance creation and store the Private IP address
   if command -v aws &> /dev/null; then
-    IP_ADDRESS=$(aws ec2 run-instances \
-      --image-id $IMAGE_ID \
-      --instance-type $INSTANCE_TYPE \
-      --security-group-ids $SECURITY_GROUP_ID \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | \
-      jq -r '.Instances[0].PrivateIpAddress')
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     
-    if [ -z "$IP_ADDRESS" ]; then
-      echo "Failed to retrieve IP address for $i instance."
-      continue
-    fi
-    
+    # Display the created instance's IP address
     echo "Created $i instance. Private IP address: $IP_ADDRESS"
-
-    # Add DNS record to Route53
-#     aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch "$(cat <<EOF
-# {
-#   "Comment": "Add $i to Route53",
-#   "Changes": [
-#     {
-#       "Action": "CREATE",
-#       "ResourceRecordSet": {
-#         "Name": "$i.$DOMAIN_NAME",
-#         "Type": "A",
-#         "TTL": 300,
-#         "ResourceRecords": [
-#           {
-#             "Value": "$IP_ADDRESS"
-#           }
-#         ]
-#       }
-#     }
-#   ]
-# }
-# EOF
-# )"
-#     echo "DNS record for $i.$DOMAIN_NAME created successfully."
-#   else
-#     echo "AWS CLI is not found in the PATH"
+  else
+    echo "AWS CLI is not found in the PATH"
   fi
 done
